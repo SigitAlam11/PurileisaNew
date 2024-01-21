55345c0eea5ffc162d8251580d15963f84c2640a
Sigit Syamsu Alam <sigitsyamsualam@gmail.com>
2024-01-22 06:58:55 +0800

    upload

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg bg-white sticky-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="./assets/images/image 1.svg" alt="" width="100px" />
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Tentang</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Kegiatan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Gallery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Team</a>
+          </li>
+        </ul>
+        <a href="news.php" class="btn btn-outline-success ms-2">Berita</a>
+        <a href="product.php" class="btn btn-outline-success ms-2">Produk Kami</a>
+        <a href="daftar.html" class="btn btn-success ms-2">Daftar Sekarang</a>
+      </div>
+    </div>
+  </nav>
